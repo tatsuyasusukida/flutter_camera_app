2039a271d060c44edd185f7becac1011f2e3be1b
susukida <susukida@loremipsum.co.jp>
2023-01-09 12:54:12 +0900

    Add VideoRecorderPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -58,7 +59,11 @@ class VideoListPage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => const VideoRecorderPage(),
+          ));
+        },
         tooltip: 'カメラを起動',
         child: const Icon(Icons.camera_alt),
       ),
@@ -188,12 +193,13 @@ class _VideoDetailPageState extends State<VideoDetailPage> {
               ? []
               : [
                   Center(
-                    child: RawMaterialButton(
+                    child: ElevatedButton(
                       onPressed: _togglePlaying,
-                      elevation: 2.0,
-                      fillColor: Theme.of(context).primaryColor,
-                      padding: const EdgeInsets.all(15.0),
-                      shape: const CircleBorder(),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        padding: const EdgeInsets.all(15.0),
+                        shape: const CircleBorder(),
+                      ),
                       child: const Icon(
                         Icons.play_arrow,
                         size: 30.0,
@@ -215,5 +221,106 @@ class _VideoDetailPageState extends State<VideoDetailPage> {
         _videoPlayerController.play();
       }
     });
+  }
+}
+
+class VideoRecorderPage extends StatefulWidget {
+  const VideoRecorderPage({super.key});
+
+  @override
+  State<VideoRecorderPage> createState() => _VideoRecorderPageState();
+}
+
+class _VideoRecorderPageState extends State<VideoRecorderPage> {
+  late CameraController _cameraController;
+  late Future<void> _initializeCameraFuture;
+  bool _isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeCameraFuture = _initializeCamera();
+  }
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initializeCamera() async {
+    final cameras = await availableCameras();
+    // final cameraFront = cameras.firstWhere((camera) {
+    //   return camera.lensDirection == CameraLensDirection.front;
+    // });
+
+    final cameraBack = cameras.firstWhere((camera) {
+      return camera.lensDirection == CameraLensDirection.back;
+    });
+
+    _cameraController = CameraController(cameraBack, ResolutionPreset.max);
+    await _cameraController.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initializeCameraFuture,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Stack(
+          children: [
+            Positioned.fill(
+              child: CameraPreview(_cameraController),
+            ),
+            Positioned.fill(
+              bottom: 40,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child:
+                    _isRecording ? _stopButton(context) : _startButton(context),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  _startButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          _isRecording = true;
+        });
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+        padding: const EdgeInsets.all(20),
+        shape: const CircleBorder(),
+      ),
+      child: const Icon(Icons.circle, size: 35),
+    );
+  }
+
+  _stopButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          _isRecording = false;
+        });
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.red,
+        padding: const EdgeInsets.all(20),
+        shape: const CircleBorder(),
+      ),
+      child: const Icon(Icons.square_rounded, size: 35),
+    );
   }
 }
